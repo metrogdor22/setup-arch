b1666a7170f232a56d6bb1eb15618e3627a98e5e
metrogdor22 <drew.hutchins1@gmail.com>
2021-08-11 09:55:29 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 pacman -R --noconfirm yakuake thunderbird qbittorrent
 pacman -S --noconfirm net-tools konsole gedit gparted okular discord transmission-qt cura steam
-pacman -Syu
+pacman -Syu --noconfirm
