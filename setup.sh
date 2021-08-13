3c70d9f84fbce7264f35d63bba07e0705627969a
metrogdor22 <drew.hutchins1@gmail.com>
2021-08-13 08:21:51 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,24 @@
-pacman-mirrors --fasttrack
+read -p "Full setup (No for Lite setup)? [Yes] " setupType
+setupType=${setupType:-Yes}
+
+echo "##### REMOVING SOFTWARE #####"
 pacman -R --noconfirm yakuake thunderbird qbittorrent
-pacman -S --noconfirm net-tools konsole gedit gparted okular discord transmission-qt cura steam
+
+echo "##### ENABLING AUR #####"
+pacman -S --needed --noconfirm base-devel
+
+echo "##### INSTALLING LITE SOFTWARE #####"
+pacman -S --noconfirm net-tools konsole gedit gparted
+
+
+if [ "$setupType" = "Yes"]; then
+  echo "##### UPDATING MIRRORS #####"
+  pacman-mirrors --fasttrack
+
+  echo "##### INSTALLING HEAVY SOFTWARE #####"
+  pacman -S --noconfirm okular discord transmission-qt cura steam
+fi
+
+
+echo "##### UPDATING SYSTEM #####"
 pacman -Syu --noconfirm
